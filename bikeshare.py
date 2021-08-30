8b2b514ccfb75126d879d11a2d859417a18339aa
Lauren Ferezy <lferezy@gmail.com>
2021-08-30 17:29:49 -0400

    comment keys for data

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,8 @@
 import time
 import pandas as pd
 import numpy as np
+
+#keys for data 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
